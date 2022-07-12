d0ebb4506c53f2171ee8367811397e76708e07f7
WankeeMan <109173032+WankeeMan@users.noreply.github.com>
2022-07-12 21:44:43 +0300

    Add files via upload

---
+++ SuspendProc/dllmain.cpp
@@ -0,0 +1,50 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <Windows.h>
+#include <iostream>
+#include <thread>
+#include <TlHelp32.h>
+
+EXTERN_C
+{
+    __declspec(dllexport) void _cdecl Suspend(DWORD procID) 
+    {
+        HANDLE hThreadSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+
+        THREADENTRY32 threadEntry;
+        threadEntry.dwSize = sizeof(THREADENTRY32);
+
+        Thread32First(hThreadSnapshot, &threadEntry);
+
+    do
+    {
+        if (threadEntry.th32OwnerProcessID == procID)
+        {
+            HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, FALSE,
+                threadEntry.th32ThreadID);
+            SuspendThread(hThread);
+            CloseHandle(hThread);
+        }
+    } while (Thread32Next(hThreadSnapshot, &threadEntry));
+
+    CloseHandle(hThreadSnapshot);
+}
+    
+    }
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
